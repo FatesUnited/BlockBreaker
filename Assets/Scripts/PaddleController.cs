43bc8f855558eba54775bbf8dfdb1f405cad82ae
Tribeman <FatesUnited@gmail.com>
2014-01-29 22:00:39 -0600

    Added walls and a controllable paddle

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	float maxSpeed = 300;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+	}
+}
+		                      
